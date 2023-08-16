4758d73b1df43fd8fd5314017c109cd1f604d7a2
nihal tekin <nihalpolattekin@gmail.com>
2023-08-16 15:39:46 +0200

    first commit

---
+++ api/tutorials/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from .models import Tutorial
+
+class TutorialSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tutorial
+        fields = "__all__"
+
+    def validate(self, data):
+        if data["title"] == data["description"]:
+            raise serializers.ValidationError({"message": "Title and description must not be same!"})
+        return data      
+
+
